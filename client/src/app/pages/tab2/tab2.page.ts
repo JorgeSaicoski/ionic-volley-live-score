3855b5c01b0e26fa5e73a857f6bb10aff90e504a
Jorge Sarkis <jorgesaicoski@gmail.com>
2024-09-07 23:22:13 -0300

    create page working

--- client/src/app/pages/tab2/tab2.page.ts
+++ client/src/app/pages/tab2/tab2.page.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { Match } from '../../models/match';
+import { MatchService } from '../../services/matches.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 
 @Component({
@@ -7,10 +11,50 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [IonicModule, CommonModule, ReactiveFormsModule]
+
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  match: Match = new Match(0, [], false, false, new Date(), '');
+  showMessage: boolean = false
+  message: string = ""
+  myForm: FormGroup;
+  
+  constructor(private matchService: MatchService) {
+    this.myForm = new FormGroup({
+      adversary: new FormControl('', [Validators.required,  Validators.minLength(3)]), 
+      matchDate: new FormControl('', Validators.required),
+    })
+  }
 
-  constructor() {}
+  ngOnInit() {
+  }
 
+  createMatch() {
+    if (this.myForm.valid){
+      this.matchService.createMatch(this.match).subscribe({
+        next: (createdMatch: any) => {
+          console.log('Match created:', createdMatch);
+          this.displayMessage('Match created successfully!');
+        },
+        error: (err: any) => {
+          console.error('Error creating match:', err);
+          this.displayMessage('Error creating match.');
+        },
+      });
+    }
+
+  }
+
+  displayMessage(message: string) {
+    this.message = message;
+    this.showMessage = true;
+
+    setTimeout(() => {
+      this.showMessage = false;
+    }, 5000);
+  }
 }
+
+
+
